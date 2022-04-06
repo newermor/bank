f1c23e31a4e5bc0a2a69f713c79dc115c092649e
newermore <Newermore@mail.com>
2022-04-06 17:23:15 +0500

    first commit

---
+++ script/main.js
@@ -0,0 +1,30 @@
+// let ismlar = ["bobir", "sobir", "nodir"];
+// const ul = document.createElement("ul");
+
+// ismlar.forEach(function (ism) {
+//   let li = document.createElement("li");
+//   li.innerText = "ism";
+//   ul.append(li);
+// });
+
+// const div = document.querySelector("#div");
+// div.append(ul);
+
+// const paagraflar = document.querySelector("p");
+// paragraflar.forEach(function (p) {
+//   if (paragraflar.innerText.includes("success")) {
+//     paragraflar.classList.add("success");
+//   } else {
+//     paragraflar.classList.add("error");
+//   }
+// });
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", function () {
+//   const mainDiv = document.querySelector("#main");
+//   mainDiv.remove();
+// });
+
+
+// if(document.readyState === "complete"){
+
+// }
